579c3ffa1144602bc05649a49043df4d4e406795
henrique <henrique.araujo62@etec.sp.gov.br>
2024-11-30 08:56:15 -0300

    first commit

---
+++ aula_02/primeiraAtt.py
@@ -0,0 +1,9 @@
+# ctrl + k + c = comentar em bloco
+# ctrl + k + u = descomentar em bloco
+# Para recuar shift + tab
+
+print("Hello word!")
+print('\nSeja bem vindo!')
+print("ola:\"henrique\"")
+
+
